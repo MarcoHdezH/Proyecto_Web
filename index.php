e6848273eae6cc0d5f62746b8d71c781130f4b9c
Marco Hernandez <marco7245@hotmail.com>
2023-03-29 18:35:39 -0600

    Iniciando Proyecto

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/build/css/app.css">
+    <title>Basílica San Pedro</title>
+</head>
+<body>
+    <header class="header">
+        <div class="titulo-header">
+            <a href="/index.php"><h1>Basílica de San Pedro</h1></a>
+            <nav class="navegacion">
+                <a href="#">¿Quienés Somos?</a>
+                <a href="#">Anuncios</a>
+                <a href="#">Nuestro Blog</a>
+                <a href="#">Contacto</a>
+                <a href="#">Crear Cuenta</a>
+            </nav>
+    </header>
+
+    <main>
+        <section class="imagen-presentacion">
+            <h2>Conferencia General de abril de 2023</h2>
+            <p>Únete a Nosotros en un servicio de adoración mundial virtual, con mensajes centrados en Jesucristo, los dias 1 y 2 de abril</p>
+        </section>
+
+        <section class="contenedor anuncios">
+            <div class="anuncio">
+                <h3>La Iglesia de Jesucristo envía alimentos, tiendas de campaña, productos sanitarios y otros artículos a Turquía y Siria</h3>
+                <p>La ayuda a las víctimas del terremoto supera los cinco millones de dólares (4 695 000 €)</p>
+            </div>
+
+            <div>
+                <h2>Ana María Bonny es llamada a representar a la Iglesia en las Naciones Unidas</h2>
+                <p>Ana María Bonny Hernández es la nueva representante de la Iglesia de Jesucristo de los Santos de los Últimos Días ante el Comité de las ONG sobre la Condición Jurídica y Social de la Mujer en las Naciones Unidas, Ginebra</p>
+            </div>
+
+            <div>
+                <h2>El mensaje del Salvador para ti</h2>
+                <p>Si Jesús estuviera aquí y ahora, les expresaría su profundo amor. Les diría que pueden lograrlo, que pueden volverse a Él, dejar atrás sus pecados. Cambiar para que Él pudiera perdonarlos.</p>
+            </div>
+        </section>
+
+    </main>
+</body>
+</html>
